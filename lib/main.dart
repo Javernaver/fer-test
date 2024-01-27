cf22efd4372cede25c8e3f6bf65a7d6c63171035
Javier del Canto <jdelcanto@imaginex.cl>
2024-01-26 23:39:57 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:fer_test/beer_list_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Infinite Scroll Pagination Sample',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: BeerListScreen(),
+      );
+}
